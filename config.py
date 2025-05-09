bc22c348c3f39b0c1ae4d431067638dae4fd11a0
Arroz2807 <nicolasmanescau07@gmail.com>
2025-05-09 11:48:13 -0300

    Todo comentado

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import os
+import os  # Módulo para manejar rutas del sistema operativo
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__)) # Define la ruta absoluta del directorio actual
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'biblioteca.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'biblioteca.db') # URI para conectar con la base de datos SQLite en el archivo biblioteca.db   
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Desactiva el seguimiento de modificaciones (reduce el consumo de memoria)
